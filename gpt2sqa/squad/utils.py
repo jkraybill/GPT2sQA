fd111037d9a3c6d5136d92d43120a9d44630bfd4
jkraybill <jeremy@seedwing.com>
2019-12-18 13:22:32 +1100

    Update utils.py

--- gpt2sqa/squad/utils.py
+++ gpt2sqa/squad/utils.py
@@ -95,7 +95,8 @@ def convert_examples_to_features(examples, tokenizer, max_seq_length,
     unique_id = 1000000000
 
     features = []
-    
+
+    total_missed = 0
     for (example_index, example) in enumerate(examples):
         query_tokens = tokenizer.tokenize(example.question_text)
 
